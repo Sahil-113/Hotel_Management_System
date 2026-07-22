715e1999f5b9702e821f758aaa78cbd3308ef5d8
Sahil Singh <0173cs221113@gmail.com>
2026-07-22 09:03:21 +0530

    Add comment with author name in Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import java.sql.PreparedStatement;
 import java.util.Scanner;
 
 public class Main {
+    // Sahil Singh
     private static final String url = "jdbc:mysql://localhost:3306/collegedb";
     private static final String user = "root";
     private static final String password = "12345678";
